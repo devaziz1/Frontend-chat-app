d0ab52227030d279feaa58e750776ee60cb2e896
devaziz1 <devaziz1@gmail.com>
2024-03-15 02:32:14 +0500

    frontend code is complete

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <ChatProvider>
+          <App />
+        </ChatProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,37 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+import Chatbox from "../components/Chatbox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
 
-function ChatPage() {
-  // const [chats, setChats] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchChats = async () => {
-  //     try {
-  //       const { data } = await axios.get("/api/chats");
-  //       setChats(data);
-  //       console.log(data);
-  //     } catch (error) {
-  //       console.error("Error fetching chats:", error);
-  //     }
-  //   };
-
-  //   fetchChats();
-
-    
-  //   return () => {
-      
-  //   };
-  // }, []); 
+const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
 
   return (
-    <div>
-      {/* {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))} */}
-
-      Chats page
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
     </div>
   );
-}
+};
 
-export default ChatPage;
+export default Chatpage;
